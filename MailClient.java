134b98a7d8e5ad69dedab53299af77e6176e368b
Bea <bea_riego93@hotmail.com>
2014-11-24 12:58:26 +0100

    Modificamos la clase MailClient a nuestra manera

--- MailClient.java
+++ MailClient.java
@@ -8,26 +8,40 @@
 public class MailClient
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private MailServer server;
+    private String user;
 
     /**
      * Constructor for objects of class MailClient
      */
-    public MailClient()
+    public MailClient(MailServer Server, String User)
     {
         // initialise instance variables
-        x = 0;
+        server = Server;
+        user = User;
+    }
+    
+    public MailItem getNextMailItem()
+    {
+        return server.getNextMailItem(user);
+    }
+    
+    public void printNextMailItem()
+    {
+        MailItem item = server.getNextMailItem(user);
+        if(item == null) {
+            System.out.println("No hay mensajes nuevos");
+        }
+        else {
+            item.print();
+        }
+        
+    }
+    
+    public void sendMailItem(String to, String message)
+    {
+        MailItem item = new MailItem(user, to, message);
+        server.post(item);
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
